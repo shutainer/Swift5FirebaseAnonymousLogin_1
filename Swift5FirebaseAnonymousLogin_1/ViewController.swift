826ea7141b01091420d9cf1da58a715e8556498f
shutainer <shutainer@yahoo.co.jp>
2020-07-02 15:34:46 +0900

    Initial Commit

---
+++ Swift5FirebaseAnonymousLogin_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5FirebaseAnonymousLogin_1
+//
+//  Created by 須藤英隼 on 2020/07/02.
+//  Copyright © 2020 Eishun Sudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
